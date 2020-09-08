de843583761c1acad680e4060d4c480f2112b696
Niyuby Blanco <nblanco.nb17@gmail.com>
2020-09-08 17:44:18 -0430

    modificaciones

--- scripts/avatar_share.js
+++ scripts/avatar_share.js
@@ -1,21 +1,19 @@
-const SHOW = document.getElementById('showOptions');
-const HIDE = document.getElementById('hideOptions');
+const VIEW = document.getElementById('showOptions');
+const OCCULT = document.getElementById('hideOptions');
 const MODAL = document.getElementsByClassName('modal')[0];
-const SHARE = document.getElementById('share')
-HIDE.onclick = () => {
-    MODAL.classList.remove('show')
+const SOCIAL = document.getElementById('share')
+OCCULT.onclick = () => {
+    SOCIAL.classList.remove('show')
     SHARE.setAttribute('images', './images/icon-share.svg')
-    MODAL.classList.remove('show')
+    SOCIAL.classList.remove('show')
 }
 
-SHOW.onclick = (e) => {
+VIEW.onclick = (e) => {
     if(MODAL.classList.contains('show')){
-        SHARE.setAttribute('images', './images/icon-share.svg')
+        SOCIAL.setAttribute('images', './images/icon-share.svg')
         MODAL.classList.remove('show')
-        SHOW.classList.remove("black")
     }else{
-        SHARE.setAttribute('images', './images/icon-shareWhite.svg')
+        SOCIAL.setAttribute('images', './images/icon-shareWhite.svg')
         MODAL.classList.add('show')
-        SHOW.classList.add("black")
     }
 }
